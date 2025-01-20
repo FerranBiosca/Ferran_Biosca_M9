0811ee226a3a2b2dbe4da8c1e71e918fcf8644d7
Ferran_Biosca <2023_ferran.biosca@iticbcn.cat>
2025-01-20 20:44:29 +0100

    activitats classes 13 i 20

--- classe_13_01/notes.php
+++ classe_13_01/notes.php
@@ -14,10 +14,17 @@ else {
     $query= mysqli_query($conn, $sql);
     $rows = mysqli_num_rows($query);
     echo "Hi ha $rows alumnes amb més d'un $nota";*/
-
     #Per insertar un usuari:
-    $insert_sql = "INSERT INTO usuaris(`dni`, `nom`, `cognom`, `nota`) VALUES ('48968288M','Pedro','Picapiedra','2')";
-    $query_insert = mysqli_query($conn, $insert_sql);
+    /*$insert_sql = "INSERT INTO usuaris(`dni`, `nom`, `cognom`, `nota`) VALUES ('99908461M','Pep,'Sánchez,'5')";
+    #$query_insert = mysqli_query($conn, $insert_sql);
+    $affected_rows= mysqli_affected_rows($conn);
+    echo "Hi ha $affected_rows afectacions a la BBDD";*/
+
+    $sql="SELECT * FROM `usuaris` WHERE nota >= 5";
+    $query=mysqli_query($conn, $sql);
+    while( $row = mysqli_fetch_array($query)) {
+        echo "Nom: " . $row['nom'] . ", cognom: " . $row['cognom'] . ", nota: " . $row['nota'];
+    }
 
 }
 ?>
